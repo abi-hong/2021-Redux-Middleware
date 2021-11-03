0703d2ff08e16ae014e43e3069b5387d8b8b9c9a
abi-hong <68041042+abi-hong@users.noreply.github.com>
2021-11-03 19:03:30 +0900

    Week5

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_KEY = "ab41980c0047292655e874cff7655a36";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchweather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
